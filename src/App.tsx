618efeaa774c001b052c8c8875b6d1edeff7a325
Greta <asth0010@elev.techcollege.dk>
2026-02-09 13:50:51 +0100

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Today } from "./pages/today";
+import { ByDate } from "./pages/bydate";
+import { Since } from "./pages/since";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Today />} />
+          <Route path="/by-date" element={<ByDate />} />
+          <Route path="/since" element={<Since />} />
+        </Routes>
+      </BrowserRouter>
+      ,
+    </>
+  );
 }
 
 export default App;
